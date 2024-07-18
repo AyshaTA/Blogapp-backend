40ffe170d629440b39fb8f5a8cf75043fe87bed2
AyshaTA <ayshata2019@gmail.com>
2024-07-18 22:42:01 +0530

    project start

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const Jsonwebtoken= require( "jsonwebtoken")
 
 let app= Express()
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(Express.json())
+app.use(Cors())
+
+
+app.post("/signup",(req,res)=>{
+    let data=req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password=hashedPassword
+    console.log(data)
 })
 
 app.listen(3030,()=>{
